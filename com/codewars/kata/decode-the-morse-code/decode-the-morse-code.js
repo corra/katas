4ad3b56db0f11765531c175fcea53483dec81eb2
Morgan <corra@users.noreply.github.com>
2017-01-12 17:07:32 -0800

    Add MORSE_CODE table and move logic to class
    - MORSE_CODE table has been defined as a constant property MorseCode.MORSE_CODE
    - Basic logic has been moved to MorseCode
    - Use MorseCode Object instance to solve Kata

--- com/codewars/kata/decode-the-morse-code/decode-the-morse-code.js
+++ com/codewars/kata/decode-the-morse-code/decode-the-morse-code.js
@@ -1,85 +1,101 @@
-decodeMorse = function(morseCode){
-  let message = morseCode.split(/\s/).reduce( (myMessage, code, currentIndex, array) => {
-      return myMessage + ((typeof MORSE_CODE[code] === 'undefined') ? ' ' : MORSE_CODE[code]);
-    }, "");
-      
-  return message.trim().replace(/\s+/g, ' ');
-}
-
 /**
  * Morse Code class
  *
  */
 class MorseCode {
-
+  constructor(inputString = '') {
+    this.message = inputString.trim();
+  }
+  
   encode(message) {
   
   }
   
+  /**
+   * Decode dot-dash encoded morse code messages and return the decoded message
+   *
+   * @param {string} encodedMessage Dot-Dash encoded morse code
+   * @return {string} 
+   */
   decodeDotDash(encodedMessage) {
-  
+    let message = encodedMessage.split(/\s/).reduce( (myMessage, code, currentIndex, array) => {
+      return myMessage + ((typeof MorseCode.MORSE_CODE[code] === 'undefined') ? ' ' : MorseCode.MORSE_CODE[code]);
+    }, "");
+    
+    this.message = message.trim().replace(/\s+/g, ' ');
+    
+    return this.message;
   }
 }
 
+Object.defineProperty(MorseCode, 'MORSE_CODE', {
+        enumerable: true,
+        configurable: false,
+        writable: false,
+        value: { 
+          '-.-.--': '!',
+          '.-..-.': '"',
+          '...-..-': '$',
+          '.-...': '&',
+          '.----.': '\'',
+          '-.--.': '(',
+          '-.--.-': ')',
+          '.-.-.': '+',
+          '--..--': ',',
+          '-....-': '-',
+          '.-.-.-': '.',
+          '-..-.': '/',
+          '-----': '0',
+          '.----': '1',
+          '..---': '2',
+          '...--': '3',
+          '....-': '4',
+          '.....': '5',
+          '-....': '6',
+          '--...': '7',
+          '---..': '8',
+          '----.': '9',
+          '---...': ':',
+          '-.-.-.': ';',
+          '-...-': '=',
+          '..--..': '?',
+          '.--.-.': '@',
+          '.-': 'A',
+          '-...': 'B',
+          '-.-.': 'C',
+          '-..': 'D',
+          '.': 'E',
+          '..-.': 'F',
+          '--.': 'G',
+          '....': 'H',
+          '..': 'I',
+          '.---': 'J',
+          '-.-': 'K',
+          '.-..': 'L',
+          '--': 'M',
+          '-.': 'N',
+          '---': 'O',
+          '.--.': 'P',
+          '--.-': 'Q',
+          '.-.': 'R',
+          '...': 'S',
+          '-': 'T',
+          '..-': 'U',
+          '...-': 'V',
+          '.--': 'W',
+          '-..-': 'X',
+          '-.--': 'Y',
+          '--..': 'Z',
+          '..--.-': '_',
+          '...---...': 'SOS',
+        },
+      }
+    ); // Provide morse code table
 
-/*
-  // TODO: Add MORSE_CODE with Object.defineProperty()
-  // Morse Code map
-   MORSE_CODE = [ 
-      '-.-.--': '!',
-      '.-..-.': '"',
-      '...-..-': '$',
-      '.-...': '&',
-      '.----.': '\'',
-      '-.--.': '(',
-      '-.--.-': ')',
-      '.-.-.': '+',
-      '--..--': ',',
-      '-....-': '-',
-      '.-.-.-': '.',
-      '-..-.': '/',
-      '-----': '0',
-      '.----': '1',
-      '..---': '2',
-      '...--': '3',
-      '....-': '4',
-      '.....': '5',
-      '-....': '6',
-      '--...': '7',
-      '---..': '8',
-      '----.': '9',
-      '---...': ':',
-      '-.-.-.': ';',
-      '-...-': '=',
-      '..--..': '?',
-      '.--.-.': '@',
-      '.-': 'A',
-      '-...': 'B',
-      '-.-.': 'C',
-      '-..': 'D',
-      '.': 'E',
-      '..-.': 'F',
-      '--.': 'G',
-      '....': 'H',
-      '..': 'I',
-      '.---': 'J',
-      '-.-': 'K',
-      '.-..': 'L',
-      '--': 'M',
-      '-.': 'N',
-      '---': 'O',
-      '.--.': 'P',
-      '--.-': 'Q',
-      '.-.': 'R',
-      '...': 'S',
-      '-': 'T',
-      '..-': 'U',
-      '...-': 'V',
-      '.--': 'W',
-      '-..-': 'X',
-      '-.--': 'Y',
-      '--..': 'Z',
-      '..--.-': '_',
-      '...---...': 'SOS'
-    ];
-  */
+decodeMorse = function(morseCode) {
+  let m = new MorseCode(morseCode);
+  
+  return m.decodeDotDash(morseCode);
+}
+
+  
